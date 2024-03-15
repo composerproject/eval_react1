5bc37a83bf081d112c0274345e635c05426f2fe8
Charlotte Varlet <varlet.charlotte@gmail.com>
2024-03-15 11:16:10 +0100

    initial state

--- src/reducer/expReducer.jsx
+++ src/reducer/expReducer.jsx
@@ -1,12 +1,58 @@
 
 export const initialState = {
-    favorite: ''
+    // input values
+    title: '',
+    amount: 0,
+    categories: [
+        { 
+            name: 'Alimentation',
+            total: 12,
+            expenses: [
+                {
+                    title: 'Ramen',
+                    amount: 12
+                }
+            ]
+        },
+        { 
+            name: 'Logement',
+            total: 0,
+            expenses: []
+        },
+        {   
+            name: 'Transport',
+            total: 0,
+            expenses: []
+        },
+        {   
+            name: 'Divertissement',
+            total: 0,
+            expenses: []
+        },
+        {   
+            name: 'Santé',
+            total: 0,
+            expenses: []
+        },
+        { 
+            name: 'Education',
+            total: 0,
+            expenses: []
+        },
+        { 
+            name: 'Autres',
+            total: 0,
+            expenses: []
+        }
+    ]
 }
   
 export const reducer = (state, action) => {
 switch (action.type) {
+    
+
     default:
-    return initialState;
+        return initialState;
 }
 }
   
